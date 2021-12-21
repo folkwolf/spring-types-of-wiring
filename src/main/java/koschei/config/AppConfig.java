6e6f350052f8aecfd4f929908bcf271b40050132
folkwolf <ibragimoffer@yandex.ru>
2021-12-21 22:02:51 +0500

    task_2.1.4

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,6 +1,8 @@
 package koschei.config;
 
+import koschei.models.Egg6;
 import koschei.models.Island2;
+import koschei.models.Needle7;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,5 +15,12 @@ public class AppConfig {
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+
+    @Bean
+    public Egg6 getEgg(Needle7 needle7) {
+        Egg6 egg = new Egg6();
+        egg.setNeedle(needle7);
+        return egg;
     }
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -2,8 +2,14 @@ package koschei.models;
 
 public class Egg6 {
 
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
+    }
+
+    private Needle7 needle;
+
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
